a1897307f8d9698412a3da42f57b8200baae3073
Hansh31 <anilchinna13@gmail.com>
2025-01-05 03:43:57 +0530

    new message

---
+++ 09-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=${id -u}
+
+#check user is root ot not
+
+if [ $USERID -ne 0]; then
+     echo "user is not a root and installation abort"
+     exit 1
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]; then 
+   echo " installing git is failing"
+else 
+    echo "installing git is success"
+fi
